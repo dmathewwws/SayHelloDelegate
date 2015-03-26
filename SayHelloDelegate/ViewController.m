0f9f3b9d86544ac09a57c6c6b0f8f3df740efee8
dannylmathews <dannyl.mathews@gmail.com>
2015-03-26 15:56:22 -0700

    Initial Commit

---
+++ SayHelloDelegate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SayHelloDelegate
+//
+//  Created by Daniel Mathews on 2015-03-26.
+//  Copyright (c) 2015 ca.lighthouselabs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
